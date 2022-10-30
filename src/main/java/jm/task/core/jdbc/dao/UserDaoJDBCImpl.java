a23a8665f8ea3a007beee4ca948cd5cfe686b82c
daramorozova619@gmail.com <111707563+DariaMorozova44@users.noreply.github.com>
2022-10-30 14:27:19 +0300

    message

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -12,74 +12,92 @@ import java.util.ArrayList;
 public class UserDaoJDBCImpl implements UserDao {
     private  final Connection connection = Util.getConnection();
 
-    public UserDaoJDBCImpl() {
+    public UserDaoJDBCImpl()  {
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
             try (Statement statement = connection.createStatement()) {
+                connection.setAutoCommit(false);
                 statement.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
                         "(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), lastName VARCHAR(45), age INT)");
+                connection.commit();
             } catch (SQLException e) {
                 e.printStackTrace();
+                connection.rollback();
             }
         }
 
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException {
             try (Statement statement =connection.createStatement()) {
+                connection.setAutoCommit(false);
                 statement.executeUpdate("DROP TABLE IF EXISTS users");
+                connection.commit();
             } catch (SQLException e) {
                 e.printStackTrace();
+                connection.rollback();
             }
         }
 
 
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         try (PreparedStatement pstm = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)")) {
+            connection.setAutoCommit(false);
             pstm.setString(1, name);
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
             pstm.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            connection.rollback();
         }
     }
 
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
         try (PreparedStatement pstm = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            connection.setAutoCommit(false);
             pstm.setLong(1, id);
             pstm.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            connection.rollback();
         }
     }
 
 
 
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
         List<User> users = new ArrayList<>();
 
         try (ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users")) {
+            connection.setAutoCommit(false);
             while(resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
                         resultSet.getString("lastName"), resultSet.getByte("age"));
                 user.setId(resultSet.getLong("id"));
                 users.add(user);
             }
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            connection.rollback();
         }
 
         return users;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
         try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
             statement.executeUpdate("TRUNCATE TABLE users");
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            connection.rollback();
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,34 +2,57 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getConnection;
+
 public class UserServiceImpl implements UserService {
+
     UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-    public void createUsersTable() {
+
+    public UserServiceImpl()  {
+    }
+
+    public void createUsersTable() throws SQLException {
         userDaoJDBC.createUsersTable();
 
+
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException {
         userDaoJDBC.dropUsersTable();
+
+
     }
 
 
-    public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name,lastName,age);
-    }
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        userDaoJDBC.saveUser(name, lastName, age);
 
-    public void removeUserById(long id) {
+        }
+
+
+
+    public void removeUserById(long id) throws SQLException {
         userDaoJDBC.removeUserById(id);
+
+
     }
 
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
+        userDaoJDBC.getAllUsers();
+
+
 
         return userDaoJDBC.getAllUsers();
+
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
         userDaoJDBC.cleanUsersTable();
+
     }
 }
